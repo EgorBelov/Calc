12b7aaf42f5d79cc0931f69026d4d9a990e7c6ae
Егор Белов <e1belov@yandex.ru>
2023-03-17 15:17:56 +0500

    q

---
+++ Cacl.WebServer/DAL/Entities/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿namespace CaclApi.DAL.Entities
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public int? ProductQuantity { get; set; }
+        public string Description { get; set; }
+        public int? ProductId { get; set; }      // внешний ключ
+        public Product? Product { get; set; }    // навигационное свойство
+
+        public List<Meal> Meals { get; set; } = new();
+       
+    }
+}
